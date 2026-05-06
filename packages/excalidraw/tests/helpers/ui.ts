da3f47a589838828f51e7d5c59fae21663a2da05
endiselmanaj <endi.selmanaj@softup.co>
2026-05-06 09:29:56 +0200

    feat: implement lasso selection tool

--- packages/excalidraw/tests/helpers/ui.ts
+++ packages/excalidraw/tests/helpers/ui.ts
@@ -429,7 +429,7 @@ const proxy = <T extends ExcalidrawElement>(
 /** Tools that can be used to draw shapes */
 type DrawingToolName = Exclude<
   ToolType,
-  "lock" | "selection" | "eraser"
+  "lock" | "selection" | "eraser" | "lasso"
 >;
 
 type Element<T extends DrawingToolName> = T extends "line" | "freedraw"
